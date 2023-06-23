f483ac9c90c460367d25fc27e331d52fa9272b14
LilinaG <lgutierrezfer@gmail.com>
2023-06-23 14:06:15 +0200

    add latest news

---
+++ src/molecules/news/Newscovid.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import '../news/newsCovid.css'
+import blog1 from '../../assets/Imgenes Comprimidas/blog-1.jpg'
+import blog2 from '../../assets/Imgenes Comprimidas/blog-2.jpg'
+import blog3 from '../../assets/Imgenes Comprimidas/blog-3.jpg'
+
+function Newscovid() {
+
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        const url = 'https://api.nytimes.com/svc/topstories/v2/world.json?api-key=ouojEXD0rC4Z63zoTPDY41GWWqU6cscY';
+
+        
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          setArticles(data.results);
+        });
+    }, []);
+
+    const displayedArticles = articles.slice(6,9);
+          
+    return (
+
+    <div className='articlesContainer'>
+    {displayedArticles.map(article => (
+
+      <div className='itemsContainer' key={article.title}>
+        <a className='text' href={article.url}>
+        <img className='img' src={article.multimedia[2].url} alt="Article Image" />
+        {article.title}
+        </a>
+      </div>
+
+
+    ))}
+  </div>
+      );
+    }
+   
+
+export default Newscovid;
