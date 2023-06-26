7758491e5f3dfa74cdc26b875e9439ff9b6b611e
Jimena <jimenacastellanos81@gmail.com>
2023-06-26 18:36:08 +0200

    Landing components v.2.3

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,13 +2,19 @@
 
 import Banner from "../../molecules/Banner/Banner"
 import AppFeaturesSection from "../../molecules/appFeaturesSection/AppFeaturesSection"
-
-
 import Faqs from "../../molecules/faqs/Faqs"
+import Newscovid from "../../molecules/news/Newscovid"
+import Footer from "../../molecules/footer/Footer"
 
 function LandingPage() {
   return (
+    <>
     <Banner></Banner>
+    <AppFeaturesSection></AppFeaturesSection>
+    <Faqs></Faqs>
+    <Newscovid></Newscovid>
+    <Footer></Footer>
+    </>
   )
 }
 
