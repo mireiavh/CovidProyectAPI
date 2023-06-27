741170ea8ab7412b89aec5982548e59d34ad9b68
mireiavh <mirvazher02@gmail.com>
2023-06-27 08:39:43 +0200

    Develop: restructured Landing Page v.3.0

--- src/molecules/faqs/Faqs.jsx
+++ src/molecules/faqs/Faqs.jsx
@@ -7,7 +7,7 @@ return (
 	<>
 	<section className="faqs-div">
 		<div className='faqs'>
-			<p>Faqs</p>
+			<h3>Faqs</h3>
 			<h2>Frequently Asked Questions</h2>
 		</div>
 		<div className='descriptions-faqs'>
@@ -17,8 +17,18 @@ return (
 				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
 				consequat.
 			</p>
-				<h5>02. We deliver the highest quality design</h5>
-				<h5>03. We deliver the highest quality design</h5>
+			<h5>02. We deliver the highest quality design</h5>
+			<p className='lorem-ipsum-one'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+				consequat.
+			</p>
+			<h5>03. We deliver the highest quality design</h5>
+			<p className='lorem-ipsum-one'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+				consequat.
+			</p>
 		</div>
 
 	</section>
