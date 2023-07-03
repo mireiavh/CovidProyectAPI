6a050dc1403601c8d0e5434e235137443c7859ad
Jimena <jimenacastellanos81@gmail.com>
2023-07-03 22:05:24 +0200

    Develop: nabvar tracker added v3.7

--- src/components/molecules/SectionCardTracker1/AppCardTracker1.jsx
+++ src/components/molecules/SectionCardTracker1/AppCardTracker1.jsx
@@ -2,17 +2,17 @@ import "./AppCardTracker1.css"
 import CardTracker1 from "../../atoms/CardTracker1/CardTracker1"
 import covidDefault from "../../../assets/icons/covid-defult.svg"
 import covidRed from "../../../assets/icons/covid-red.svg"
-import covidGreen from "../../../assets/icons/covid-green.svg"
-import covidBlue from "../../../assets/icons/covid-blue.svg"
-import covidOrange from"../../../assets/icons/covid-orange.svg"
-import covidRedDark from "../../../assets/icons/covid-redark.svg"
+//import covidGreen from "../../../assets/icons/covid-green.svg"
+//import covidBlue from "../../../assets/icons/covid-blue.svg"
+//import covidOrange from"../../../assets/icons/covid-orange.svg"
+//import covidRedDark from "../../../assets/icons/covid-redark.svg"
 import useFetch from "../../../hooks/useFetch"
 import DynamicUrl from "../../../services/DynamicUrl"
 
 
 
 function AppCardTracker1() {
-    const data = useFetch(`${DynamicUrl}/all`);
+    const data = useFetch(`${DynamicUrl}/countries`);
     return ( 
         <>
             <section className="cardT1Container">   
@@ -20,10 +20,10 @@ function AppCardTracker1() {
                     <div className="CardT1">
                         <CardTracker1  image={covidDefault} contentTitle={"Total Cases"} text= "default" contentNumber={data.cases}/>
                     </div>
-                    <div className="CardT1">
+                    { <div className="CardT1">
                         <CardTracker1  image={covidRed} contentTitle={"Total Deaths"}  text= "red" contentNumber={data.deaths}/>
                     </div>
-                    <div className="CardT1">
+                    /*<div className="CardT1">
                         <CardTracker1  image={covidGreen} contentTitle={"Total Recovered"}  text= "green" contentNumber={data.recovered}/>
                     </div>
                     <div className="CardT1">
@@ -34,7 +34,7 @@ function AppCardTracker1() {
                     </div>
                     <div className="CardT1">
                         <CardTracker1  image={covidRedDark} contentTitle={"New Deaths"}  text= "redDark" contentNumber={data.todayDeaths}/>
-                    </div>
+                    </div> */}
                 </div>    
             </section>  
 
